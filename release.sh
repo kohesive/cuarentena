f74e4cf26281be1c98ca0dc418019267b790e5fa
apatrida <jayson.minard@gmail.com>
2017-11-20 21:42:55 -0200

    gradle revamping

--- release.sh
+++ release.sh
@@ -1,2 +1,7 @@
-./gradlew clean build check uploadArchives closeAndReleaseRepository --no-daemon --max-workers=1
+set -e
+./gradlew --stop
+./gradlew clean build check
+set +e
+./gradlew binTray --no-daemon  --max-workers=1
+./gradlew uploadArchives closeAndReleaseRepository --no-daemon  --max-workers=1
 
